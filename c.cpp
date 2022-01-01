ceeb5565470c0427cb38e524a6dac126a23058ce
Athif binu <athifbinu@gmail.com>
2022-01-01 08:21:57 +0530

    new project

---
+++ c.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int choice;
+	printf("1 for chicken \n 2 for chicken \n 3 for rice \n Enter your choice");
+	scanf("%d",&choice);
+	switch(choice){
+	case 1:
+		printf("you are selected chicken");
+		break;
+	case 2:
+		printf("you are selected chicken");
+		break;
+	case 3:
+		printf("you are selected rice");
+		break;
+	default:
+		printf("Athif is hacked you");
+	}
+
+
+
+
+
+
+
+
+	return 0;
+}
