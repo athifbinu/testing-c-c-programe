aa3156ee8180215ba26e715d4754b80e948026e4
Athif binu <85118365+athifbinu@users.noreply.github.com>
2022-01-01 23:17:56 +0530

    Update sum.cpp
    to add sum value

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number1,number2,sum;
+	printf("Enter a 2 number");
+    scanf("%d%d",&number1,&number2);
+    sum=number1+number2;
+    printf("result is  %d",sum);
+
+	return 0;
+}
